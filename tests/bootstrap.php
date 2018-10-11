c6907b6599a0ecb4c18b5d052ea5d0349310fd49
Paweł Brzozowski <pawel@positive.codes>
2018-10-11 18:54:25 +0200

    moved to src

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -10,4 +10,4 @@ $_SERVER['SCRIPT_FILENAME'] = __FILE__;
 require_once __DIR__ . '/../vendor/autoload.php';
 require_once __DIR__ . '/../vendor/yiisoft/yii2/Yii.php';
 
-Yii::setAlias('@bizley/migration', __DIR__ . '/../');
+Yii::setAlias('@bizley/migration', __DIR__ . '/../src/');
