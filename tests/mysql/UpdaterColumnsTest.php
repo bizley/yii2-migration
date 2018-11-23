37ea02b307eeab4ad191c1f0c3fd5bfc1fbf5486
Paweł Brzozowski <pawel@positive.codes>
2018-11-23 19:33:02 +0100

    travis dbg

--- tests/mysql/UpdaterColumnsTest.php
+++ tests/mysql/UpdaterColumnsTest.php
@@ -30,4 +30,32 @@ class UpdaterColumnsTest extends \bizley\tests\cases\UpdaterColumnsTestCase
         $this->assertArrayHasKey('col_int', $updater->plan->alterColumn);
         $this->assertEquals(9, $updater->plan->alterColumn['col_int']->length);
     }
+
+    /**
+     * @runInSeparateProcess
+     * @preserveGlobalState disabled
+     * @throws \yii\base\ErrorException
+     * @throws \yii\db\Exception
+     * @throws \yii\base\NotSupportedException
+     */
+    public function testChangeSizeGeneral(): void
+    {
+        $this->dbUp('test_columns');
+
+        Yii::$app->db->createCommand()->alterColumn('test_columns', 'col_int', $this->integer(9))->execute();
+
+        $updater = $this->getUpdater('test_columns');
+
+        $updater->isUpdateRequired();
+// Travis frakery
+        $this->assertEmpty($updater->plan->addColumn);
+        $this->assertEmpty($updater->plan->addForeignKey);
+        $this->assertEmpty($updater->plan->addPrimaryKey);
+        $this->assertEmpty($updater->plan->alterColumn);
+        $this->assertEmpty($updater->plan->createIndex);
+        $this->assertEmpty($updater->plan->dropColumn);
+        $this->assertEmpty($updater->plan->dropForeignKey);
+        $this->assertEmpty($updater->plan->dropIndex);
+        $this->assertEmpty($updater->plan->dropPrimaryKey);
+    }
 }
