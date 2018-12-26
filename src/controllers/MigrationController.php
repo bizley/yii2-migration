496a7e36b328dc1e1ae50414713e8e3320456e51
Júnior Pires <juniorpiresupe@gmail.com>
2018-12-26 12:06:48 -0300

    fix: change utf8mb4 to utf8 to avoid error 'field is to long'

--- src/controllers/MigrationController.php
+++ src/controllers/MigrationController.php
@@ -128,7 +128,7 @@ class MigrationController extends Controller
      */
     public $tableOptionsInit = '$tableOptions = null;
         if ($this->db->driverName === \'mysql\') {
-            $tableOptions = \'CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci ENGINE=InnoDB\';
+            $tableOptions = \'CHARACTER SET utf8 COLLATE utf8_unicode_ci ENGINE=InnoDB\';
         }';
 
     /**
