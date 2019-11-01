3833b1b7c4eb422279b1fb9e2c184f8558da6fb9
Bizley <pawel@positive.codes>
2019-11-01 14:12:01 +0100

    Foreign key migration new line breaks

--- src/views/create_fk_migration.php
+++ src/views/create_fk_migration.php
@@ -23,9 +23,8 @@ class <?= $className ?> extends Migration
     public function up()
     {
 <?php foreach ($fks as $fk): ?>
-<?= $fk->render() ?>
+<?= $fk->render() . "\n" ?>
 <?php endforeach; ?>
-
     }
 
     public function down()
