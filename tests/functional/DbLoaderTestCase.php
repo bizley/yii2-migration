5e325e6d56e2ae4b6e10f55073f163abcde6f3f9
Bizley <pawel@positive.codes>
2020-04-03 11:15:49 +0200

    Tests

--- tests/functional/DbLoaderTestCase.php
+++ tests/functional/DbLoaderTestCase.php
@@ -5,14 +5,55 @@ declare(strict_types=1);
 namespace bizley\tests\functional;
 
 use yii\db\Connection;
+use yii\db\Exception;
 use yii\db\SchemaBuilderTrait;
 
 abstract class DbLoaderTestCase extends DbTestCase
 {
     use SchemaBuilderTrait;
 
+    /** @var string */
+    private $table;
+
+    /** @var string */
+    public static $tableOptions;
+
     protected function getDb(): Connection
     {
         return static::$db;
+    }
+
+    protected function setUp(): void
+    {
+        $this->table = null;
+    }
+
+    /**
+     * @throws Exception
+     */
+    protected function tearDown(): void
+    {
+        $this->dropTable();
+    }
+
+    /**
+     * @param string $table
+     * @param array $columns
+     * @throws Exception
+     */
+    protected function createTable(string $table, array $columns): void
+    {
+        $this->getDb()->createCommand()->createTable($table, $columns, static::$tableOptions)->execute();
+        $this->table = $table;
+    }
+
+    /**
+     * @throws Exception
+     */
+    private function dropTable(): void
+    {
+        if ($this->table !== null) {
+            $this->getDb()->createCommand()->dropTable($this->table)->execute();
+        }
     }
 }
--- tests/functional/GeneratorTest.php
+++ tests/functional/GeneratorTest.php
@@ -4,7 +4,70 @@ declare(strict_types=1);
 
 namespace bizley\tests\functional;
 
-final class GeneratorTest extends DbLoaderTestCase
-{
+use bizley\tests\stubs\MigrationControllerStub;
+use Yii;
+use yii\base\InvalidRouteException;
+use yii\console\Exception as ConsoleException;
+use yii\console\ExitCode;
+use yii\db\Exception;
 
+/*abstract */class GeneratorTest extends DbLoaderTestCase
+{
+    /** @var MigrationControllerStub */
+    private $controller;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+        $this->controller = new MigrationControllerStub('migration', Yii::$app);
+        MigrationControllerStub::$stdout = '';
+        MigrationControllerStub::$confirmControl = true;
+    }
+
+    /**
+     * @test2
+     * @throws Exception
+     */
+    public function shouldGenerateGeneralSchemaTable(): void
+    {
+        $this->createTable(
+            'gs_columns',
+            [
+                'id' => $this->primaryKey(),
+                'col_big_int' => $this->bigInteger(),
+                'col_int' => $this->integer(),
+                'col_small_int' => $this->smallInteger(),
+                'col_bin' => $this->binary(),
+                'col_bool' => $this->boolean(),
+                'col_char' => $this->char(),
+                'col_date' => $this->date(),
+                'col_date_time' => $this->dateTime(),
+                'col_decimal' => $this->decimal(),
+                'col_double' => $this->double(),
+                'col_float' => $this->float(),
+                'col_money' => $this->money(),
+                'col_string' => $this->string(),
+                'col_text' => $this->text(),
+                'col_time' => $this->time(),
+                'col_timestamp' => $this->timestamp(),
+            ]
+        );
+
+
+    }
+
+    /**
+     * @test
+     * @throws InvalidRouteException
+     * @throws ConsoleException
+     */
+    public function shouldHandleNonExistingTable(): void
+    {
+        $this->assertEquals(ExitCode::DATAERR, $this->controller->runAction('create', ['non-existing-table']));
+
+        $this->assertStringContainsString(
+            '',
+            MigrationControllerStub::$stdout
+        );
+    }
 }
