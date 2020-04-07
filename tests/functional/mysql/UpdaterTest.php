bdef95378369615a2e42274bb77c0b2747b8ae1f
Bizley <pawel@positive.codes>
2020-04-07 13:41:26 +0200

    4.0

--- tests/functional/mysql/UpdaterTest.php
+++ tests/functional/mysql/UpdaterTest.php
@@ -125,4 +125,30 @@ class UpdaterTest extends \bizley\tests\functional\UpdaterTest
             MigrationControllerStub::$content
         );
     }
+
+    /**
+     * @test
+     * @throws ConsoleException
+     * @throws InvalidRouteException
+     * @throws Exception
+     */
+    public function shouldUpdateTableByAddingColumnAsFirst(): void
+    {
+        $this->addBase();
+        $this->getDb()->createCommand()->addColumn('updater_base', 'added', $this->integer()->first())->execute();
+
+        $this->assertEquals(ExitCode::OK, $this->controller->runAction('update', ['updater_base']));
+        $this->assertStringContainsString(
+            'public function up()
+    {
+        $this->addColumn(\'{{%updater_base}}\', \'added\', $this->integer()->first());
+    }
+
+    public function down()
+    {
+        $this->dropColumn(\'{{%updater_base}}\', \'added\');
+    }',
+            MigrationControllerStub::$content
+        );
+    }
 }
